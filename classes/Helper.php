a07401451002acf8c25793f6474e92d0f8163d29
Max Schmitt <max.andreas.schmitt@googlemail.com>
2017-07-02 14:02:58 +0200

    Updated closing tags according to the HTML5 specs

--- classes/Helper.php
+++ classes/Helper.php
@@ -44,7 +44,7 @@ class Helper {
 
         if($settings->enable_canonical_url)
         {
-            return '<link rel="canonical" href="'. Request::url().'"/>';
+            return '<link rel="canonical" href="'. Request::url().'">';
         }
 
         return "";
@@ -71,20 +71,20 @@ class Helper {
         {
             $ogTags = "";
             if($settings->og_fb_appid)
-                $ogTags  .= '<meta property="fb:app_id" content="'.$settings->og_fb_appid.'" />' ."\n" ;
+                $ogTags  .= '<meta property="fb:app_id" content="'.$settings->og_fb_appid.'">' ."\n" ;
 
             if($settings->og_sitename)
-                $ogTags  .= '<meta property="og:site_name" content="'.$settings->og_sitename .'" />'."\n" ;
+                $ogTags  .= '<meta property="og:site_name" content="'.$settings->og_sitename .'">'."\n" ;
 
             if($post->seo_description)
-                $ogTags  .= '<meta property="og:description" content="'.$post->seo_description.'" />'."\n" ;
+                $ogTags  .= '<meta property="og:description" content="'.$post->seo_description.'">'."\n" ;
 
             $ogTitle = empty($post->meta_title) ? $post->title : $post->meta_title;
             $ogUrl = empty($post->canonical_url) ? Request::url() : $this->page->canonical_url ;
 
-            $ogTags .= '<meta property="og:title" content="'. $ogTitle .'" />'."\n" ;
+            $ogTags .= '<meta property="og:title" content="'. $ogTitle .'">'."\n" ;
 
-            $ogTags .= '<meta property="og:url" content="'. $ogUrl .'" />';
+            $ogTags .= '<meta property="og:url" content="'. $ogUrl .'">';
 
             return $ogTags;
         }
